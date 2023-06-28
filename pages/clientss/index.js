24ba1382ab824a2579cf8bbd58bee8ce5e9d465a
seyoung97 <seyoung1st@gmail.com>
2023-06-28 16:34:56 +0900

    동적 라우트로 내비게이팅하기

--- pages/clientss/index.js
+++ pages/clientss/index.js
@@ -1,12 +1,23 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 function ClientsPage() {
-  const router = useRouter();
-  console.log(router.query);
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
 
   return (
     <div>
       <h1>The Clients Page!</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clientss/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
