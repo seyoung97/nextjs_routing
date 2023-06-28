a309676eff57182cea4dc14c156617ae31611f0b
seyoung97 <seyoung1st@gmail.com>
2023-06-28 16:19:43 +0900

    Link 컴포넌트로 네비게이팅하기

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clientss">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
